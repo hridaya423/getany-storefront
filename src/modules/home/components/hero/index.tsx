a1f9faa4d83c26762486551b379c0fae8cf18f72
Hridaya Agrawal <hridayahoney@gmail.com>
2022-10-23 22:23:32 +0530

    Add files via upload

---
+++ src/modules/home/components/hero/index.tsx
@@ -0,0 +1,31 @@
+import UnderlineLink from "@modules/common/components/underline-link"
+import Image from "next/image"
+
+const Hero = () => {
+  return (
+    <div className="h-[90vh] w-full relative">
+      <div className="text-white absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:text-left small:justify-end small:items-start small:p-32">
+        <h1 className="text-2xl-semi mb-4 drop-shadow-md shadow-black">
+          Halloween is here! 🎃🦇
+        </h1>
+        <p className="text-base-regular max-w-[32rem] mb-6 drop-shadow-md shadow-black">
+          10% off on all products!
+        </p>
+        <UnderlineLink href="/store">Explore products</UnderlineLink>
+      </div>
+      <Image
+        src="/Halloween.png"
+        layout="fill"
+        loading="eager"
+        priority={true}
+        quality={90}
+        objectFit="cover"
+        alt="Photo by @thevoncomplex https://unsplash.com/@thevoncomplex"
+        className="absolute inset-0"
+        draggable="false"
+      />
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/modules/layout/components/footer-cta/index.tsx
@@ -0,0 +1,8 @@
+import UnderlineLink from "@modules/common/components/underline-link"
+import Image from "next/image"
+
+const FooterCTA = () => {
+  return <div className="bg-amber-100 w-full"></div>
+}
+
+export default FooterCTA
