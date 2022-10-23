829bc65bf2c168321bb74e75aaf108eacedfd27e
Hridaya Agrawal <hridayahoney@gmail.com>
2022-10-23 22:12:23 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,16 @@
+const { withStoreConfig } = require("./store-config")
+const store = require("./store.config.json")
+
+module.exports = withStoreConfig({
+  features: store.features,
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "medusa-public-images.s3.eu-west-1.amazonaws.com",
+      "localhost",
+      "192.168.1.25",
+    ],
+  },
+})
+
+console.log("next.config.js", JSON.stringify(module.exports, null, 2))
